348a33b90685f5fbed19923014be843113cc9d2d
Jeffrey Santana <accounts@jeffreysantana.ch>
2020-11-14 15:05:18 +0100

    working on contactperson function :construction:

---
+++ includes/contactperson.inc.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Infomation 
+Created by: Jeffrey Santana
+Last edited by : Jeffrey Santana
+AppVersion: 0.1
+FileVersion: 0.1
+
+Function: Get Data out of table contactperson
+*/
+
+function get_contactperson($id)
+{
+    require 'hostinfos.cred.php';
+
+    $array = array();
+    $sql = "SELECT * from contactperson where contactperson_id = ? ";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        return $array;
+        exit();
+    } else {
+        #insert the date into table
+        mysqli_stmt_bind_param($stmt, "i", $id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $id, $firstname,$lastname,$jobdescription, $email, $phone, $company);
+                while (mysqli_stmt_fetch($stmt)) {
+            $returnarray = array('contactperson_id' => $id, 'contactperson_firstname' => $firstname, 'contactperson_lastname' => $lastname,'contactperson_jobdescription' => $jobdescription, 'contactperson_email' => $email, 'contactperson_phone' => $phone, 'contactperson_company' => $company );
+        }
+        return $returnarray;
+    }
+}
+function get_contactpersons()
+{
+    require 'hostinfos.cred.php';
+
+    $array = array();
+    $sql = "SELECT * from contactperson";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $array[] = $row;
+    }
+    $returnarray = array('contactpersons' => $array);
+    return $returnarray;
+}
