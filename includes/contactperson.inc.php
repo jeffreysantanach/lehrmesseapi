0c85ee66edee4b520526a3ab431d15b0cb028261
Jeffrey Santana <accounts@jeffreysantana.ch>
2020-11-14 16:24:52 +0100

    edit get_contactperson function :construction:

--- includes/contactperson.inc.php
+++ includes/contactperson.inc.php
@@ -28,16 +28,24 @@ function get_contactperson($id)
         return $returnarray;
     }
 }
-function get_contactpersons()
+function get_contactpersons($company_id)
 {
     require 'hostinfos.cred.php';
-
     $array = array();
-    $sql = "SELECT * from contactperson";
-    $result = mysqli_query($conn, $sql);
-    while ($row = mysqli_fetch_assoc($result)) {
-        $array[] = $row;
-    }
+    $sql = "SELECT * from contactperson where contactperson_company = ? ";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        return $array;
+        exit();
+    } else {
+        #insert the date into table
+        mysqli_stmt_bind_param($stmt, "i", $company_id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $id, $firstname, $lastname, $jobdescription, $email, $phone, $company);
+        while (mysqli_stmt_fetch($stmt)) {
+            $array[$id] = array('contactperson_id' => $id, 'contactperson_firstname' => $firstname, 'contactperson_lastname' => $lastname, 'contactperson_jobdescription' => $jobdescription, 'contactperson_email' => $email, 'contactperson_phone' => $phone, 'contactperson_company' => $company);
+        }
     $returnarray = array('contactpersons' => $array);
     return $returnarray;
+}
 }
