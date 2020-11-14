73ce4fef2a7a2a03e65a30bd96f35fb713b7bad2
Jeffrey Santana <accounts@jeffreysantana.ch>
2020-11-14 14:35:30 +0100

    improve format of the code :art:

--- includes/apprenticeship.inc.php
+++ includes/apprenticeship.inc.php
@@ -9,39 +9,37 @@ FileVersion: 0.1
 Function: Get Data out of table apprenticeship
 */
 
-function get_apprenticeship($id){
-require 'hostinfos.cred.php';
-	
-$array = array();
-$sql = "SELECT * from apprenticeship where apprenticeship_id = ? ";
-$stmt = mysqli_stmt_init($conn);
-if (!mysqli_stmt_prepare($stmt, $sql)) {
-    return $array;
-    exit();
-} else {
-    #insert the date into table
-    mysqli_stmt_bind_param($stmt, "i", $id);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_bind_result($stmt,$id,$profession,$company);
-	while (mysqli_stmt_fetch($stmt)) {
-	$returnarray = array('apprenticeship_id' =>$id, 'apprenticeship_profession'=> $profession, 'apprenticeship_company'=>$company);
-	}
-return $returnarray;
-}
+function get_apprenticeship($id)
+{
+    require 'hostinfos.cred.php';
 
+    $array = array();
+    $sql = "SELECT * from apprenticeship where apprenticeship_id = ? ";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        return $array;
+        exit();
+    } else {
+        #insert the date into table
+        mysqli_stmt_bind_param($stmt, "i", $id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $id, $profession, $company);
+        while (mysqli_stmt_fetch($stmt)) {
+            $returnarray = array('apprenticeship_id' => $id, 'apprenticeship_profession' => $profession, 'apprenticeship_company' => $company);
+        }
+        return $returnarray;
+    }
 }
 function get_apprenticeships()
 {
-require 'hostinfos.cred.php';
-	
-$array = array();
-$sql = "SELECT * from apprenticeship";
-$result = mysqli_query($conn,$sql);
-while ($row = mysqli_fetch_assoc($result)) {
-	$array[] = $row ;
-}
-$returnarray = array('apprenticeships' =>$array);
-return $returnarray;
-}
+    require 'hostinfos.cred.php';
 
-
+    $array = array();
+    $sql = "SELECT * from apprenticeship";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $array[] = $row;
+    }
+    $returnarray = array('apprenticeships' => $array);
+    return $returnarray;
+}
--- includes/profession.inc.php
+++ includes/profession.inc.php
@@ -9,38 +9,36 @@ FileVersion: 0.1
 Function: Get Data out of table profession
 */
 
-function get_profession($id){
-require 'hostinfos.cred.php';
-$array = array();
-$sql = "SELECT * from profession where profession_id = ? ";
-$stmt = mysqli_stmt_init($conn);
-if (!mysqli_stmt_prepare($stmt, $sql)) {
-    return $array;
-    exit();
-} else {
-    #insert the date into table
-    mysqli_stmt_bind_param($stmt, "i", $id);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_bind_result($stmt,$id,$name,$description,$field);
-	while (mysqli_stmt_fetch($stmt)) {
-	$returnarray = array('profession_id' =>$id, 'profession_name'=> $name, 'profession_description'=>$description,'profession_field'=>$field);
-	}
-return $returnarray;
-}
-
+function get_profession($id)
+{
+    require 'hostinfos.cred.php';
+    $array = array();
+    $sql = "SELECT * from profession where profession_id = ? ";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        return $array;
+        exit();
+    } else {
+        #insert the date into table
+        mysqli_stmt_bind_param($stmt, "i", $id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $id, $name, $description, $field);
+        while (mysqli_stmt_fetch($stmt)) {
+            $returnarray = array('profession_id' => $id, 'profession_name' => $name, 'profession_description' => $description, 'profession_field' => $field);
+        }
+        return $returnarray;
+    }
 }
 function get_professions()
 {
-require 'hostinfos.cred.php';
-	
-$array = array();
-$sql = "SELECT * from profession";
-$result = mysqli_query($conn,$sql);
-while ($row = mysqli_fetch_assoc($result)) {
-	$array[] = $row ;
-}
-$returnarray = array('professions' =>$array);
-return $returnarray;
-}
+    require 'hostinfos.cred.php';
 
-
+    $array = array();
+    $sql = "SELECT * from profession";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $array[] = $row;
+    }
+    $returnarray = array('professions' => $array);
+    return $returnarray;
+}
