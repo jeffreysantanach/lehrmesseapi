22f3988cbc9f3af2a7b38adf4904b10561e4f919
Jeffrey Santana <accounts@jeffreysantana.ch>
2020-11-14 15:15:55 +0100

    added function company by booth :construction:

--- includes/company.inc.php
+++ includes/company.inc.php
@@ -43,3 +43,24 @@ function get_companies()
     $returnarray = array('companies' => $array);
     return $returnarray;
 }
+function get_company_booth($booth)
+{
+    require 'hostinfos.cred.php';
+
+    $array = array();
+    $sql = "SELECT * from company where company_booth = ? ";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        return $array;
+        exit();
+    } else {
+        #insert the date into table
+        mysqli_stmt_bind_param($stmt, "i", $id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $id, $name, $adress, $email, $phone, $booth, $description, $location);
+        while (mysqli_stmt_fetch($stmt)) {
+            $returnarray = array('company_id' => $id, 'company_name' => $name, 'company_adress' => $adress, 'company_email' => $email, 'company_phone' => $phone, 'company_booth' => $booth, 'company_description' => $description, 'company_location' => $location);
+        }
+        return $returnarray;
+    }
+}
