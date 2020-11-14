9db118b09ec2e64ef702ce5bc00082a681ac6a0c
Jeffrey Santana <accounts@jeffreysantana.ch>
2020-11-14 17:39:35 +0100

    finished call function :rocket:

---
+++ includes/calls.inc.php
@@ -0,0 +1,93 @@
+<?php
+/*
+Infomation 
+Created by: Jeffrey Santana
+Last edited by : Jeffrey Santana
+
+
+Function: Get Data out of table contactperson
+*/
+
+require 'apprenticeship.inc.php';
+require 'company.inc.php';
+require 'contactperson.inc.php';
+require 'profession.inc.php';
+require 'town.inc.php';
+
+
+#companies
+
+function companies()
+{
+    $companies = get_companies();
+    $companies = $companies['companies'];
+    $returnarray = array();
+    foreach ($companies as $company) {
+        $town = get_town($company['company_location']);
+        $town = $town['town_name'];
+        $array = array(
+            'name' => $company['company_name'],
+            'email' => $company['company_email'],
+            'phone' => $company['company_phone'],
+            'street' => $company['company_adress'],
+            'postalcode' => $company['company_location'],
+            'city' => $town,
+            'website' => $company['company_website'],
+            'booth' => $company['company_booth']
+        );
+        $returnarray[$company['company_id']] = $array;
+    }
+
+    return $returnarray;
+}
+
+function company($id)
+{
+    $company = get_company($id);
+    $town = get_town($company['company_location']);
+    $town = $town['town_name'];
+    $contactpersons = get_contactpersons($id);
+    $apprenticeships = get_apprenticeships_of_company($id);
+    $apprenticeships = $apprenticeships['apprenticeships'];
+    foreach ($apprenticeships as $apprenticeship) {
+        $profession = get_profession($apprenticeship['apprenticeship_profession']);
+        $apprenticearray[$apprenticeship['apprenticeship_id']] = array(
+            'name' => $profession['profession_name'],
+            'description' => $profession['profession_description']
+        );
+    }
+    $returnarray = array(
+        'name' => $company['company_name'],
+        'email' => $company['company_email'],
+        'phone' => $company['company_phone'],
+        'street' => $company['company_adress'],
+        'postalcode' => $company['company_location'],
+        'city' => $town,
+        'website' => $company['company_website'],
+        'contactpersons' => $contactpersons,
+        'apprenticeships' => $apprenticearray,
+        'booth' => $company['company_booth']
+
+    );
+    return $returnarray;
+}
+
+function apprenticeships()
+{
+    $apprenticeships = get_apprenticeships();
+    $apprenticeships = $apprenticeships['apprenticeships'];
+    $array = array();
+    foreach ($apprenticeships as $apprenticeship) {
+        $profession_id = $apprenticeship['apprenticeship_profession'];
+        $key_exists = array_key_exists($profession_id, $array);
+        if (!$key_exists) {
+            $profession = get_profession($profession_id);
+            $array[$profession_id] = array(
+                'name' => $profession['profession_name'],
+                'description' => $profession['profession_description']
+            );
+        }
+    }
+    $returnarray = array('apprenticeships' => $array);
+    return $returnarray;
+}
