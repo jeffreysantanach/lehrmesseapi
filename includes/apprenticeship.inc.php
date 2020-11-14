d4b07c90bdc59e8a41efcad6517e5bdc613ac031
Jeffrey Santana <accounts@jeffreysantana.ch>
2020-11-14 16:46:00 +0100

    add new function ✨

--- includes/apprenticeship.inc.php
+++ includes/apprenticeship.inc.php
@@ -42,3 +42,25 @@ function get_apprenticeships()
     $returnarray = array('apprenticeships' => $array);
     return $returnarray;
 }
+function get_apprenticeships_of_company($company_id)
+{
+    require 'hostinfos.cred.php';
+
+    $array = array();
+    $sql = "SELECT * from apprenticeship where apprenticeship_company = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        return $array;
+        exit();
+    } else {
+        #insert the date into table
+        mysqli_stmt_bind_param($stmt, "i", $company_id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $id, $profession, $company);
+        while (mysqli_stmt_fetch($stmt)) {
+            $array[$id] = array('apprenticeship_id' => $id, 'apprenticeship_profession' => $profession, 'apprenticeship_company' => $company);
+        }
+        $returnarray = array('apprenticeships' => $array);
+    return $returnarray;
+}
+}
